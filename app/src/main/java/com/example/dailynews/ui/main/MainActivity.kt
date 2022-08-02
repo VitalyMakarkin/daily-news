04ff8f87feafa44a16730961830220b9790fbd28
proff116 <mak.nk16rus@gmail.com>
2022-08-02 10:15:07 +0300

    Add main application files

---
+++ app/src/main/java/com/example/dailynews/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.dailynews.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dailynews.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
