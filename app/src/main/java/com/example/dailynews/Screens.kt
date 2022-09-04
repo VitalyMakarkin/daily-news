8141fe3742b97c37135dd6fbe0f714804dd5fc1a
vitalymakarkin <mak.nk16rus@gmail.com>
2022-09-04 11:56:48 +0300

    Add shared module

--- app/src/main/java/com/example/dailynews/Screens.kt
+++ app/src/main/java/com/example/dailynews/Screens.kt
@@ -1,15 +1,10 @@
 package com.example.dailynews
 
-import com.example.dailynews.ui.article.ArticleFragment
 import com.example.dailynews.ui.articles.ArticlesFragment
 import com.github.terrakok.cicerone.androidx.FragmentScreen
 
 object Screens {
     fun Articles() = FragmentScreen {
         ArticlesFragment()
-    }
-
-    fun Article(article: String) = FragmentScreen {
-        ArticleFragment.newInstance(article)
     }
 }
