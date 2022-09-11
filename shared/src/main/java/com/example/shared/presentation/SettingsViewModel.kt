ec1eb540c3bd910788a9636172a6384df73fde16
vitalymakarkin <mak.nk16rus@gmail.com>
2022-09-11 23:46:44 +0300

    Refactor settings view model

--- shared/src/main/java/com/example/shared/presentation/SettingsViewModel.kt
+++ shared/src/main/java/com/example/shared/presentation/SettingsViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.shared.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.shared.domain.ArticleInteractor
@@ -11,6 +13,38 @@ import javax.inject.Inject
 class SettingsViewModel @Inject constructor(
     private val articlesInteractor: ArticleInteractor
 ) : ViewModel() {
+
+    sealed class UiStateView {
+        data class Data(
+            val articlesCount: Int,
+            val cachedArticlesCount: Int,
+            val favoriteArticlesCount: Int
+        ) : UiStateView()
+
+        object Loading : UiStateView()
+        class Error(val throwable: Throwable) : UiStateView()
+    }
+
+    private var _uiStateLiveData: MutableLiveData<UiStateView> =
+        MutableLiveData(UiStateView.Loading)
+    val uiStateLiveData: LiveData<UiStateView> get() = _uiStateLiveData
+
+    init {
+        refresh()
+    }
+
+    fun refresh() {
+
+        viewModelScope.launch {
+            _uiStateLiveData.value = UiStateView.Loading
+
+            // TODO: Define articlesCount
+
+            // TODO: Define cachedArticlesCount
+
+            // TODO: Define favoriteArticlesCount
+        }
+    }
 
     fun removeCachedArticles() {
         viewModelScope.launch {
