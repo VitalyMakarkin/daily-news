651c2596499f0bd59460a0ed42252bccb04cc199
vitalymakarkin <mak.nk16rus@gmail.com>
2022-10-01 19:35:46 +0300

    Add app-compose module DI

---
+++ app-compose/src/main/java/com/example/app_compose/di/AppModule.kt
@@ -0,0 +1,19 @@
+package com.example.app_compose.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Named
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Named("IO")
+    fun provideDispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
