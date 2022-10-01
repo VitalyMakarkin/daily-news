e53b789bd99237856d24fb41c3c4f41922620b88
vitalymakarkin <mak.nk16rus@gmail.com>
2022-10-02 00:11:31 +0300

    Add coil compose

--- app-compose/src/main/java/com/example/app_compose/ui/shared/ImageArticleItem.kt
+++ app-compose/src/main/java/com/example/app_compose/ui/shared/ImageArticleItem.kt
@@ -1,9 +1,15 @@
 package com.example.app_compose.ui.shared
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.shared.presentation.model.ImageArticleUI
 
 @Composable
@@ -11,7 +17,29 @@ fun ImageArticleItem(
     modifier: Modifier = Modifier,
     article: ImageArticleUI
 ) {
-    Text(modifier = modifier, text = article.title)
+    Column(
+        modifier = modifier
+            .padding(top = 6.dp, bottom = 6.dp)
+    ) {
+        AsyncImage(
+            modifier = modifier
+                .height(200.dp)
+                .padding(top = 6.dp, bottom = 6.dp),
+            model = article.urlToImage,
+            contentDescription = "Image description",
+            contentScale = ContentScale.Crop,
+        )
+        Text(
+            modifier = modifier
+                .padding(start = 12.dp, end = 12.dp, top = 6.dp, bottom = 6.dp),
+            text = article.title
+        )
+        Text(
+            modifier = modifier
+                .padding(start = 12.dp, end = 12.dp, top = 6.dp, bottom = 6.dp),
+            text = article.publishedAt
+        )
+    }
 }
 
 @Preview(showBackground = true)
--- app-compose/src/main/java/com/example/app_compose/ui/shared/TextArticleItem.kt
+++ app-compose/src/main/java/com/example/app_compose/ui/shared/TextArticleItem.kt
@@ -1,9 +1,12 @@
 package com.example.app_compose.ui.shared
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.shared.presentation.model.TextArticleUI
 
 @Composable
@@ -11,7 +14,21 @@ fun TextArticleItem(
     modifier: Modifier = Modifier,
     article: TextArticleUI
 ) {
-    Text(modifier = modifier, text = article.title)
+    Column(
+        modifier = modifier
+            .padding(top = 6.dp, bottom = 6.dp)
+    ) {
+        Text(
+            modifier = modifier
+                .padding(start = 12.dp, end = 12.dp, top = 6.dp, bottom = 6.dp),
+            text = article.title
+        )
+        Text(
+            modifier = modifier
+                .padding(start = 12.dp, end = 12.dp, top = 6.dp, bottom = 6.dp),
+            text = article.publishedAt
+        )
+    }
 }
 
 @Preview(showBackground = true)
