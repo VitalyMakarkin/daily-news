500e7e3b3fd02807a0fac57b2e4061ed9f086bbb
vitalymakarkin <mak.nk16rus@gmail.com>
2022-10-02 23:11:17 +0300

    Update ui article screen

--- app/src/main/java/com/example/dailynews/ui/article/ArticleFragment.kt
+++ app/src/main/java/com/example/dailynews/ui/article/ArticleFragment.kt
@@ -1,12 +1,17 @@
 package com.example.dailynews.ui.article
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import coil.load
 import com.example.dailynews.databinding.FragmentArticleBinding
 import com.example.dailynews.ui.articles.ArticlesFragment
+import com.example.shared.presentation.ArticleViewModel
 import com.example.shared.presentation.ArticleViewModel.Companion.ARG_ARTICLE_ID
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,6 +28,8 @@ class ArticleFragment : Fragment() {
         }
     }
 
+    private val viewModel: ArticleViewModel by viewModels()
+
     private var _binding: FragmentArticleBinding? = null
     private val binding get() = _binding!!
 
@@ -33,6 +40,42 @@ class ArticleFragment : Fragment() {
     ): View {
         _binding = FragmentArticleBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.refreshButton.setOnClickListener {
+            viewModel.refresh()
+        }
+
+        viewModel.uiStateLiveData
+            .observe(viewLifecycleOwner) { render(it) }
+    }
+
+    private fun render(uiStateView: ArticleViewModel.UiStateView) {
+        binding.articleLayout.isVisible = uiStateView is ArticleViewModel.UiStateView.Data
+        binding.errorsLayout.isVisible = uiStateView is ArticleViewModel.UiStateView.Error
+        binding.progressIndicator.isVisible = uiStateView is ArticleViewModel.UiStateView.Loading
+
+        when (uiStateView) {
+            is ArticleViewModel.UiStateView.Data -> {
+                val article = uiStateView.article
+                binding.articleTitleTv.text = article.id.toString()
+                binding.articleAuthorTv.text = article.id.toString()
+                binding.articlePublishedAtTv.text = article.id.toString()
+                binding.articleDescriptionTv.text = article.id.toString()
+                binding.articleImageIv.load(article.id) {
+                    placeholder(ColorDrawable(0xEEEEEE))
+                    crossfade(true)
+                }
+            }
+            is ArticleViewModel.UiStateView.Error -> {
+                binding.errorsTv.text = uiStateView.throwable.message ?: "Unknown Error!"
+            }
+            is ArticleViewModel.UiStateView.Loading -> Unit
+
+        }
     }
 
     override fun onDestroyView() {
