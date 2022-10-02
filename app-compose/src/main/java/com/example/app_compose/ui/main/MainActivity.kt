261e5cf0d39c0d1cc406bf8d55c5f89bc7eff619
vitalymakarkin <mak.nk16rus@gmail.com>
2022-10-02 16:52:08 +0300

    Add navigation bar

--- app-compose/src/main/java/com/example/app_compose/ui/main/MainActivity.kt
+++ app-compose/src/main/java/com/example/app_compose/ui/main/MainActivity.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.app_compose.ui.articles.ArticlesScreen
 import com.example.app_compose.ui.theme.DailynewsTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -31,9 +40,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DailyNews() {
-    // TODO: Implement navigation
-    ArticlesScreen()
+fun DailyNews(
+    navHostController: NavHostController = rememberNavController()
+) {
+    Scaffold(bottomBar = {
+        BottomNavigation() {
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        painter = painterResource(id = com.example.app_compose.R.drawable.ic_articles_list),
+                        contentDescription = null
+                    )
+                },
+                selected = false,
+                onClick = { /*TODO*/ })
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        painter = painterResource(id = com.example.app_compose.R.drawable.ic_favorite_articles_list),
+                        contentDescription = null
+                    )
+                },
+                selected = false,
+                onClick = { /*TODO*/ })
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        painter = painterResource(id = com.example.app_compose.R.drawable.ic_settings),
+                        contentDescription = null
+                    )
+                },
+                selected = false,
+                onClick = { /*TODO*/ })
+        }
+    }) {
+        NavHost(
+            navController = navHostController,
+            startDestination = "articles",
+        ) {
+            composable("articles") {
+                ArticlesScreen()
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
